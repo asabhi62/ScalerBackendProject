3dd44d0875654d26edc92d6a63116807003f6e4d
abhinav <asabhisharma62@gmail.com>
2025-12-13 17:33:56 +0530

    Fix: Stop tracking target folder and add HelloController

---
+++ src/main/java/com/learning/backend/product/controllers/HelloController.java
@@ -0,0 +1,12 @@
+package com.learning.backend.product.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from Product Service!";
+    }
+}
